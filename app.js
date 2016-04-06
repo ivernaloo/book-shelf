c46e771f2aebc2da63a1d769da7d2d3fba4a9132
周鹏 <zhoupeng@xiaomi.com>
2016-04-06 21:46:30 +0800

    把书的目录都提交到统一文件上

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var rd = require('./lib');
+var rd = require('./lib'),
+    path = require('path'),
+    fs = require('fs');
 
 rd.read('.', function(err, files){
     if (err) throw err;
@@ -23,13 +25,29 @@ rd.each('.', function (f, s, next) {
 });
 
 // 同步遍历目录下的所有文件
-rd.eachSync('.', function (f, s) {
-    // 每找到一个文件都会调用一次此函数
-    // 参数s是通过 fs.stat() 获取到的文件属性值
-    // console.log('file: %s', f);
-});
+// rd.eachSync('.', function (f, s) {                                                       z
+//     // 每找到一个文件都会调用一次此函数
+//     // 参数s是通过 fs.stat() 获取到的文件属性值
+//     // console.log('file: %s', f);
+// });
 
 // 同步遍历目录下的所有js文件
 rd.eachFileFilterSync('D:/MyDocument/MyBank/sync/book', /\.pdf$/, function (f, s) {
-    console.log(f);
+    var bookname = path.basename(f, '.pdf');
+
+        fs.appendFile('book.txt', bookname+"\n", function (err) {
+
+    });
+
+
 });
+
+
+/*
+* @todo  文件名存储
+* */
+
+/*
+* @todo 获取分类tag：聚类或者从douban上通过api拿到数据
+*
+* */
